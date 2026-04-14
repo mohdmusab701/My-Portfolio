704e67abbb7e2c5a0094e1f6889f1c13e313d35c
webifyX <eduwebix36@gmail.com>
2026-04-15 05:10:42 +0530

    Udated

---
+++ src/components/About.tsx
@@ -0,0 +1,18 @@
+import "./styles/About.css";
+
+const About = () => {
+  return (
+    <div className="about-section" id="about">
+      <div className="about-me">
+        <h3 className="title">About Me</h3>
+        <p className="para">
+          I am a passionate software engineer student with a strong focus on creating innovative solutions. With experience in both frontend and backend development within the MERN stack with generative AI, 
+          I have a proven track record of delivering high-quality projects. I am dedicated to continuous learning and growth, always seeking new challenges to expand my skill set.
+          I thrive in collaborative environments and enjoy tackling complex problems.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/components/Career.tsx
@@ -0,0 +1,35 @@
+import "./styles/Career.css";
+
+const Career = () => {
+  return (
+    <div className="career-section section-container">
+      <div className="career-container">
+        <h2>
+          My career <span>&</span>
+          <br /> experience
+        </h2>
+        <div className="career-info">
+          <div className="career-timeline">
+            <div className="career-dot"></div>
+          </div>
+          <div className="career-info-box">
+            <div className="career-info-in">
+              <div className="career-role">
+                <h4>Student</h4>
+                
+              </div>
+              <h3>NOW</h3>
+            </div>
+            <p>
+             I am currently working full stack mern stack developer and genrative ai and learning new technologies to enhance my skills and knowledge in the field of web development. I am passionate about creating innovative and efficient solutions to real-world problems through technology.
+            </p>
+          </div>
+   
+          </div>
+        </div>
+      </div>
+    
+  );
+};
+
+export default Career;
